c2b57bfcc2487b1f7cfb44ee62b8748feb70558b
Liitzy <liitzyrodriimoree2099@gmail.com>
2021-04-14 00:39:29 -0500

    Quinto commit

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyAwhEgm_Vy4s_qy3gqw5aTIE0kN7_6Jz0M",
+    authDomain: "liitzywpa.firebaseapp.com",
+    projectId: "liitzywpa",
+    storageBucket: "liitzywpa.appspot.com",
+    messagingSenderId: "1071220339647",
+    appId: "1:1071220339647:web:6dda5eebc0f166ffa5670a",
+    measurementId: "G-8Q3C1HNVWQ"
 });
